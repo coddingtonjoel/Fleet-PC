6c123cf4c5b24bb0b3be2c06c101590f7f7c77ca
Joel Coddington <coddingtonjoel@gmail.com>
2020-04-03 17:22:25 -0700

    Removed Pace.js
    wasn't working as intended

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "pace-js";
-import "pace-js/themes/orange/pace-theme-minimal.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
